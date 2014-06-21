9deedef1ae9b7c552e9a710d728dd5c43f368291
TomyLobo <tomylobo@nurfuerspam.de>
2014-06-21 20:38:30 +0200

    Added missing velocity-tools dependency to the CSS validator.

--- build-css-validator-war.sh
+++ build-css-validator-war.sh
@@ -15,6 +15,7 @@ if [ ! -d 2002 ]; then
     sed -i '/Velocity.getLog/i }' ./2002/css-validator/org/w3c/css/index/IndexGenerator.java
     sed -i '/For each language, we set the context/i if(false) {' ./2002/css-validator/org/w3c/css/index/IndexGenerator.java
     sed -i 's/"1.6"/"1.7"/g' ./2002/css-validator/build.xml
+    sed -i 's#<copy file="[^"]*velocity[^"]*.jar" tofile="[^"]*velocity[^"]*.jar"/>#\0<get dest="lib/velocity-tools-2.0.jar" src="http://central.maven.org/maven2/org/apache/velocity/velocity-tools/2.0/velocity-tools-2.0.jar"/>#' ./2002/css-validator/build.xml
 fi;
 
 # build the jar file
